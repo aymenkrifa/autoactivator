ddf2bae10f3f4d4e0072e9addfaa5d3892293ec4
aymenkrifa <aymenkrifa@gmail.com>
2023-03-27 22:24:54 +0100

    feat: add user confirmation for creating shell file if it doesn't exit

--- install.py
+++ install.py
@@ -22,6 +22,55 @@ def is_shell_installed(shell_name: str) -> bool:
 def is_unixbased_system() -> bool:
     return any(os_value.lower() in sys.platform.lower() for os_value in POSSIBLE_OS)
 
+def user_input_confirmation(question: str, default: str = "yes") -> bool:
+    """
+    Ask a yes/no question via input() and return the answer as a boolean value.
+
+    Parameters
+    ----------
+    question : str
+        Question presented to the user in the prompt
+    default : str, optional
+        The presumed answer if the user just hits <Enter>, by default "yes"
+
+        The 'default' argument must be:
+            - "yes": <Enter> is interpreted as a yes,
+            - "no" or None: meaning an answer is required of the user
+
+    Returns
+    -------
+    bool
+        return True for "yes"
+        return False for "no"
+
+    Raises
+    ------
+    ValueError
+        If the default parameter is neither a 'yes', 'no' or None, raise a ValueError.
+    """
+
+    # Initilize a yes/no value mapper
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
+
 
 install_input = pick.pick(
     options=list(SHELL_CONFIGS.keys()),
@@ -43,9 +92,21 @@ for shell in input_shells:
         if is_shell_installed(shell):
 
             config_file = os.path.expanduser(f"~/{SHELL_CONFIGS[shell]}")
+            if not os.path.exists(config_file):
+
+                create_script_file = user_input_confirmation(
+                    f"File '{config_file}' doesn't exist, it is required for the installation on '{shell}' shell. Do you want to create it?"
+                )
+
+                if create_script_file:
+                    mode = "w+"
+                else:
+                    continue
+            else:
+                mode = "r+"
 
             # Check if the config file already sources the activator script
-            with open(config_file, "r+") as f:
+            with open(config_file, mode) as f:
                 content = f.read()
 
                 if f"source {activator_script_path}" in content:
@@ -53,11 +114,11 @@ for shell in input_shells:
 
                 else:
                     # Append the source command to the end of the config file
-
                     f.write(f"\n# The {APP_NAME.title()} script\nsource {activator_script_path}\n""")
                     print(f"Activator script sourced in {config_file}")
 
             os.system(f". {config_file}")
+
         else:
             print(f"Sorry, '{shell}' is not installed in your system.")
     else:
