0d357e039c3d98f0fa4e89cd8578365bd6aa0900
aymenkrifa <aymenkrifa@gmail.com>
2023-07-02 17:40:37 +0100

    Test: clone only develop branch to get the changes

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@
 shell="$@"
 
 TARGET_DIR="$HOME/.autoactivator"
-REPO_URL="https://github.com/aymenkrifa/autoactivator/main"
+REPO_URL="https://github.com/aymenkrifa/autoactivator"
 PYTHON_EXECUTABLE="python3"
 
 install_script_path="$TARGET_DIR/install.py"
 
 mkdir -p "$TARGET_DIR"
-git clone "$REPO_URL" "$TARGET_DIR"
+git clone --branch develop --single-branch "$REPO_URL" "$TARGET_DIR"
 
 echo -e "Installing..\n"
 "$PYTHON_EXECUTABLE" "$install_script_path" $shell
