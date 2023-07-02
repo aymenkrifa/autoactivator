adf7b7e5ea7aa307e56e5bdb8f185ac720d6cb6a
aymenkrifa <aymenkrifa@gmail.com>
2023-07-02 13:38:45 +0100

    WIP: set up shell selection through command flag

--- setup.sh
+++ setup.sh
@@ -1,39 +1,19 @@
 #!/bin/bash
 
+# Set the desired shell based on the command-line argument
+shell="$1"
+
 TARGET_DIR="$HOME/.autoactivator"
 REPO_URL="https://github.com/aymenkrifa/autoactivator"
-BRANCH="feature/host_setup_shell_script"
-PYTHON_EXECUTABLE="python3"
-SHELLS=""
 
-echo "Installing..."
-
-# Parse command line arguments
-while getopts ":s:" opt; do
-  case $opt in
-    s)
-      SHELLS="$OPTARG"
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      ;;
-  esac
-done
+install_script_path="$TARGET_DIR/install.py"
 
 # Create the target directory
 mkdir -p "$TARGET_DIR"
-
-# Clone the repository into the target directory
 git clone "$REPO_URL" "$TARGET_DIR"
 
-# Change to the cloned directory
-cd "$TARGET_DIR"
+# Change to the project directory
+cd $TARGET_DIR
 
-# Checkout the desired branch
-git checkout "$BRANCH"
-
-# Modify the shebang line of activator.sh to use bash
-sed -i 's/#!\/bin\/sh/#!\/bin\/bash/' activator.sh
-
-# Run the install.py script with the chosen Python executable and shells
-"$PYTHON_EXECUTABLE" install.py $SHELLS
+# Execute the install.py script with the chosen shell flag
+python3 "$install_script_path" "$shell"
