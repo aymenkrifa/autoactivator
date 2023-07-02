05e33a6e9b4ff7a416c7843a2481553b60b73cf3
aymenkrifa <aymenkrifa@gmail.com>
2023-07-02 13:42:29 +0100

    WIP: debug: switch to host_setip_shell_script branch instead of main

--- setup.sh
+++ setup.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# Set the desired shell based on the command-line argument
 shell="$1"
 
 TARGET_DIR="$HOME/.autoactivator"
 REPO_URL="https://github.com/aymenkrifa/autoactivator"
+BRANCH="feature/host_setup_shell_script"
+PYTHON_EXECUTABLE="python3"
 
 install_script_path="$TARGET_DIR/install.py"
 
-# Create the target directory
 mkdir -p "$TARGET_DIR"
 git clone "$REPO_URL" "$TARGET_DIR"
 
-# Change to the project directory
 cd $TARGET_DIR
+git checkout "$BRANCH"
 
-# Execute the install.py script with the chosen shell flag
-python3 "$install_script_path" "$shell"
+PYTHON_EXECUTABLE "$install_script_path" "$shell"
