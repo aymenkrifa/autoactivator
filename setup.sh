e75693459ac98d988ad93c301d8ff421f35b7080
aymenkrifa <aymenkrifa@gmail.com>
2023-07-02 17:43:49 +0100

    Change to the cloning of the main branch

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ PYTHON_EXECUTABLE="python3"
 install_script_path="$TARGET_DIR/install.py"
 
 mkdir -p "$TARGET_DIR"
-git clone --branch develop --single-branch "$REPO_URL" "$TARGET_DIR"
+git clone "$REPO_URL" "$TARGET_DIR"
 
 echo -e "Installing..\n"
 "$PYTHON_EXECUTABLE" "$install_script_path" $shell
