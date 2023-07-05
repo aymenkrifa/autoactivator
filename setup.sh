0848c309b214b960e85922b049358689700eca30
aymenkrifa <aymenkrifa@gmail.com>
2023-07-05 16:03:25 +0100

    debug: add handler for Git dependency before cloning the repository

--- setup.sh
+++ setup.sh
@@ -8,6 +8,13 @@ PYTHON_EXECUTABLE="python3"
 
 install_script_path="$TARGET_DIR/install.py"
 
+# Check if Git is installed
+if ! command -v git &> /dev/null; then
+    echo "Git is not installed. Please install it before launching the installation again."
+    echo "You can install Git from: https://git-scm.com/"
+    exit 1
+fi
+
 mkdir -p "$TARGET_DIR"
 git clone "$REPO_URL" "$TARGET_DIR"
 
