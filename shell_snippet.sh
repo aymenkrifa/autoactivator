80c764a49d4763e907fa63bb691a99fca109770a
aymenkrifa <akr@quicktext.im>
2024-02-15 19:21:54 +0100

    feat: Move the AutoActivator config script to a seperate file for an easy future debug
      - If a change is needed in a Shell config file, the update would be
        complicated as it's a file that can have changes in the meantime
      - No guarantee if the config file would be intact and the initial
        snippet placement is still the same
      - A seperate Shell file is therefore the easiest and straight-forward
        way to do it

---
+++ shell_snippet.sh
@@ -0,0 +1,30 @@
+# activator.sh
+APP_NAME="AutoActivator"
+autoactivator_folder="$HOME/.autoactivator"
+activator_path="$autoactivator_folder/activator.sh"
+
+if [ -e "$activator_path" ]; then
+    source "$activator_path"
+else
+    echo -e "\\033[1m{$APP_NAME}\\033[0m: Activator script path not found: $activator_path"
+fi
+
+autoactivator_update() {
+    if [ -d "$autoactivator_folder" ]; then
+        git --git-dir="$autoactivator_folder/.git" --work-tree="$autoactivator_folder" pull origin main
+        source "$activator_path"
+    else
+        echo -e "\\033[1m{$APP_NAME}\\033[0m: {$APP_NAME} directory not found."
+    fi
+}
+
+autoactivator() {
+    case "$1" in
+    "update")
+        autoactivator_update
+        ;;
+    *)
+        echo -e "\\033[1m{$APP_NAME}\\033[0m: Invalid command. Usage: autoactivator [update]"
+        ;;
+    esac
+}
